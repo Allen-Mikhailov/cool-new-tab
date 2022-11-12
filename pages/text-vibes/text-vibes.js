1bec6e2a20a0266e23c8bc177f151fb4858b2e9e
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-11-12 13:47:46 -0600

    Rain is now spaced out

--- pages/text-vibes/text-vibes.js
+++ pages/text-vibes/text-vibes.js
@@ -6,11 +6,16 @@ const rainDropContainer = document.createElement("div")
 
 document.body.appendChild(rainDropContainer)
 
+function getDif(i)
+{
+    return (i/rainDropCount)*125 + Math.random()*5
+}
+
 for (var i = 0; i < rainDropCount; i++)
 {
     const drop = document.createElement("div")
     drop.classList.add("drop")
-    drop.style.left = Math.random()*200+"%"
+    drop.style.left = getDif(i)+"%"
     drop.style.top = "-20px"
     drop.style.animationDelay = Math.random()*5 + "s"
 
